891daa4e429ce831b40c33d5d1af1f4c4391634a
Shamsdeenshaukat <Shaukatshamsdeen7@gmail.com>
2022-08-23 16:53:32 +0000

    secound commit

--- src/screens/Aboutscreeen.js
+++ src/screens/Aboutscreeen.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../components/Header";
 
 function Aboutscreeen() {
-  return <div>Aboutscreeen</div>;
+  return (
+    <div>
+      <Header />
+      <p />
+      This app is about countries , their region, currencies, common and
+      official name, their flag, country code, capital cities <p />
+    </div>
+  );
 }
 
 export default Aboutscreeen;
